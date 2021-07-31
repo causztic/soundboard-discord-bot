ecb6bc7d93b819cd81545d37bd6feb75811b4712
yaojie <limyaojie93@gmail.com>
2021-07-31 16:56:32 +0800

    Add discord integration

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 'use strict';
 
+const Discord = require('discord.js');
+const { OpusEncoder } = require('@discordjs/opus');
 const ioHook = require('iohook');
 
+// TODO: refactor
+
+const client = new Discord.Client();
 // key n has a keycode of n + 1
 const numberKeyCodes = Array.from({ length: 10 }, (_v, i) => i + 2);
+let voiceConnection;
 
-ioHook.on('keydown', (event) => {
-  // ctrl + cmd key
-  if (event.ctrlKey && event.metaKey) {
-    if (numberKeyCodes.includes(event.keycode)) {
-      console.log(`playing ${event.keycode-1}.mp3`);
-    }
+client.on('message', async message => {
+  if (message.content === 's!start' && message.member.voice.channel) {
+    voiceConnection = await message.member.voice.channel.join();
+
+    ioHook.on('keydown', (event) => {
+      // ctrl + cmd key
+      if (event.ctrlKey && event.metaKey) {
+        if (numberKeyCodes.includes(event.keycode)) {
+          const clip = `${event.keycode-1}.mp3`;
+          console.log(`playing ${clip}`);
+          voiceConnection.play(`clips/${clip}`);
+        }
+      }
+    });
+  
+    // Register and start hook
+    ioHook.start();
+  }
+
+  if (message.content === 's!stop') {
+    voiceConnection.disconnect();
+    ioHook.stop();
   }
 });
 
-// Register and start hook
-ioHook.start();
+client.login(process.env.TOKEN);
+
